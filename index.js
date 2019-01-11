af2fd5835b9f8bb5b04c5853246552fca9c30455
nicorponic <n1co_frago@hotmail.com>
2019-01-11 12:16:25 +0100

    Trying next question funcitonality

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const questions = {
-    category: "TV-Shows",
-    question: "Which TV show is about cooking meth?",
-    answer: " Breaking Bad"
+const data = {
+    questions: [
+        {
+            question: "Which TV show is about cooking meth?",
+            answer: " Breaking Bad"
+        },
+        {
+            question: "Which TV show is about 4 nerd friends",
+            answer: "The big bang theory"
+        },
+        {
+            question: "Which TV show is about 4 nerd friends",
+            answer: "The big bang theory"
+        },
+    
+    ]
+    
 }
 
+let i = 0;
 const paragraph = document.createElement("p");
-paragraph.innerText = questions.question;
+paragraph.innerText = data.questions[i].question;
 
 const container = document.getElementById("question-answer");
 container.appendChild(paragraph);
@@ -13,6 +27,11 @@ container.appendChild(paragraph);
 const button = document.getElementById("check-answer");
 //button.onclick(flip);
 
-function flip() {
-    paragraph.innerText = questions.answer;
+function flip(i) {
+    paragraph.innerText = data.questions[i].answer;
 }
+
+function nextQuestion(i) {
+    paragraph.innerText = data.questions.question[i++];
+    console.log(i);
+};
